6f701e187e682a92a4b38eb518ae4d112e86ea54
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2020-07-27 14:46:03 -0700

    Added Documentation for instructor class

---
+++ NestedObjects/Instructor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    /// <summary>
+    /// This class represents an instructor for a particular course.
+    /// </summary>
+    class Instructor
+    {
+        /// <summary>
+        /// Legal full name, first and last.
+        /// Example: John Doe
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// The person's email address.
+        /// Example: John.Doe@yahoo.com
+        /// </summary>
+
+        public string Email { get; set; }
+    }
+}
